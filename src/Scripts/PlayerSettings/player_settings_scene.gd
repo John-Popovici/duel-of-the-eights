42d4706705efe5780ef651d1ea83ab9402491aa9
Hemraj <bhatth14@mcmaster.ca>
2025-01-03 22:18:45 -0500

    🔨 Updated player_settings_scene script

--- src/Scripts/PlayerSettings/player_settings_scene.gd
+++ src/Scripts/PlayerSettings/player_settings_scene.gd
@@ -1,11 +1,15 @@
-extends Node
+extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
+func returnToIntro() -> void:
+	var intro_scene = load("res://Scenes/IntroScene.tscn").instantiate()
+	
+	# Change scene to intro scene
+	get_tree().root.add_child(intro_scene)
+	queue_free()  # Free IntroScene
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
