9234acb38836fc9c5860b29cccfaf9955191df9e
Nigel Moses <57297364+nigelmoses32@users.noreply.github.com>
2025-01-31 17:26:22 -0500

    🔨 Fixed toast layer for debug always on

--- src/Scripts/UniversalScripts/toast_layer.gd
+++ src/Scripts/UniversalScripts/toast_layer.gd
@@ -1,6 +1,5 @@
 extends Control
 
-@export var duration: float = 5.0  # Time before disappearing
 @export var toast: PackedScene = preload("res://NodeScene/toast.tscn")
 
 func set_message(message: String):
@@ -8,3 +7,6 @@ func set_message(message: String):
 	get_node("CanvasLayer/VBoxContainer").add_child(toast_instance)
 	print("Sending toast message P2: ", message)
 	toast_instance.set_message(message)
+
+func _ready() -> void:
+	self.visible = GlobalSettings.debug_mode
