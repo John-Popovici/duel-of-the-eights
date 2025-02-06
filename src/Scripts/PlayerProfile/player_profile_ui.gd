353ff2faa646acfe4adffee855c0f2342cbbdc32
Isaac <izzie717@gmail.com>
2025-02-06 01:40:31 -0500

    🔨 Finished profile picture feature

--- src/Scripts/PlayerProfile/player_profile_ui.gd
+++ src/Scripts/PlayerProfile/player_profile_ui.gd
@@ -16,6 +16,7 @@ signal profile_settings_ready(profile_settings)
 @onready var back_to_home_button = listingsContainer.get_node("BackToHomeButton")
 
 @export var presets_folder = "res://Presets"
+@export var player_settings_path = (presets_folder + "/profile_settings.json")
 
 var profile_settings_saved = false
 @export var profile_settings: Dictionary = {}
@@ -43,14 +44,12 @@ func _on_invert_select_method_toggled(button_pressed: bool):
 func _on_sfx_slider_drag_ended(value):
 	print("sfx volume set to: ", value)
 	set_sfx_volume(value, false)
-	AudioManager.set_sfx_volume(float(self.sfxVolume)/100)
 	# save modified sfx volume preferences
 	self.save_profile_settings()
 	
 func _on_music_slider_drag_ended(value):
 	print("music volume set to: ", value)
 	set_music_volume(value, false)
-	AudioManager.set_music_volume(float(self.musicVolume)/100)
 	# save modified music volume preferences
 	self.save_profile_settings()
 	
@@ -63,9 +62,8 @@ func _on_image_file_selected(path: String):
 	if err != OK:
 		push_error("Failed to load image at: " + path)
 		return
-		
-	var save_path = presets_folder + "/images/profile_pic.png"  # 'user://' points to the application's data folder
-	# Save the image as a PNG file
+
+	var save_path = presets_folder + "/Images/profile_pic.png"
 	var error = image.save_png(save_path)
 	if error != OK:
 		push_error("Failed to save the image: " + str(error))
@@ -75,17 +73,21 @@ func _on_image_file_selected(path: String):
 	# Create an ImageTexture, assign the loaded image to it, and give to profile pic
 	var texture = ImageTexture.new()
 	texture.set_image(image)
+	texture.set_size_override(Vector2(200,200))
 	profile_pic_button.texture_normal = texture
+	profile_pic_button.texture_hover = null
 	
 func set_sfx_volume(volume: float, setSlider: bool):
 	self.sfxVolume = volume
 	sfx_slider_value.text = str(volume) + "%"
+	AudioManager.set_sfx_volume(float(volume)/100)
 	if setSlider:
 		sfx_slider.value = volume
 
 func set_music_volume(volume: float, setSlider: bool):
 	self.musicVolume = volume
 	music_slider_value.text = str(volume) + "%"
+	AudioManager.set_music_volume(float(volume)/100)
 	if setSlider:
 		music_slider.value = volume
 
@@ -97,18 +99,55 @@ func save_profile_settings():
 		"music_volume": self.musicVolume
 	}
 
-	var file_path = presets_folder + "/profile_settings.json"
-	print("Writing to: ", file_path)
-	var file = FileAccess.open(file_path, FileAccess.WRITE)
+	print("Writing to: ", player_settings_path)
+	var file = FileAccess.open(player_settings_path, FileAccess.WRITE)
 	var json = JSON.new()
 	var json_string = json.stringify(profile_settings)
 	file.store_string(json_string)
 	file.close()
 	print("profile settings saved.")
 
-func _ready() -> void:
+# Load existing profile settings
+func load_profile_settings():
+	if not FileAccess.file_exists(player_settings_path):
+		print("no data to load")
+		return
+		
+	var file = FileAccess.open(player_settings_path, FileAccess.READ)
+	var content = file.get_as_text()
+	var json_data = JSON.parse_string(content)
+	
+	if json_data == null:
+		print("Error parsing JSON")
+		return
+	
+	# load username
+	if json_data["player_name"] != null:
+		self.username = json_data["player_name"]
+		self.username_box.text = json_data["player_name"]
+	# load selection method
+	if json_data["invert_selection_method"] != null:
+		self.invertedSelection = json_data["invert_selection_method"]
+		invert_select_method.button_pressed = self.invertedSelection
+		# load sfx_volume
+	if json_data["sfx_volume"] != null:
+		self.set_sfx_volume(json_data["sfx_volume"], true)
+		# load music_volume
+	if json_data["music_volume"] != null:
+		self.set_music_volume(json_data["music_volume"], true)
+		
+	# load profile pic
+	var save_path1 = presets_folder + "/Images/profile_pic.png"
+	if FileAccess.file_exists(save_path1):
+		self._on_image_file_selected(save_path1)
+
+# Load default settings
+func load_default_settings():
 	set_sfx_volume(int(AudioManager.get_sfx_volume()*100), true)
 	set_music_volume(int(AudioManager.get_music_volume()*100), true)
+
+func _ready() -> void:
+	self.load_default_settings()
 	
 	fileDialog.filters = ["*.png ; PNG Images", "*.jpg ; JPEG Images"]
 	fileDialog.file_selected.connect(self._on_image_file_selected)
@@ -118,6 +157,9 @@ func _ready() -> void:
 	sfx_slider.connect("value_changed", self._on_sfx_slider_drag_ended)
 	music_slider.connect("value_changed", self._on_music_slider_drag_ended)
 	back_to_home_button.connect("pressed",self.returnToIntro)
+	
+	self.load_profile_settings()
+	
 	AudioManager.connect_buttons()
 
 func returnToIntro() -> void:
