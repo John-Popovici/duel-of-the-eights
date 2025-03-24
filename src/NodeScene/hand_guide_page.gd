805128af0c741c392fb69866e221c84685decf57
STARS952 <naishan2020@gmail.com>
2025-03-24 11:08:34 -0400

    🔨 Update Guide Photos
    Updated guide photos to assist in understanding of hands. implementation

---
+++ src/NodeScene/hand_guide_page.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+
+@onready var vbox_container = $PanelContainer/HandGuideInfoBox/ScrollContainer/HandDetailsInfo
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.visible = false
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
