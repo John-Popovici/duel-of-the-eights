656ea16fda8c30b09f04f411dcc1efa7dea75832
Isaac <izzie717@gmail.com>
2025-03-26 18:38:07 -0400

    🔨 Credit Roll Code Complete

---
+++ src/NodeScene/credit_roll_ui.gd
@@ -0,0 +1,42 @@
+extends ScrollContainer
+
+@export var scroll_speed: float = 50  # Pixels per second
+@export var idle_resume_delay: float = 8.0  # Seconds before resuming after user interaction
+@export var pause_between_cycles: float = 0.5  # seconds between directions
+
+var tween: Tween
+
+
+func _start_auto_scroll():
+	if tween:
+		tween.kill()
+
+	var scroll_bar = get_v_scroll_bar()
+	var max_scroll = scroll_bar.max_value - scroll_bar.page
+	var duration = max_scroll / scroll_speed
+
+	#Loop our tweens
+	tween = create_tween().set_loops().set_trans(Tween.TRANS_LINEAR)
+	tween.tween_property(self, "scroll_vertical", max_scroll, duration).set_delay(pause_between_cycles)
+	tween.tween_property(self, "scroll_vertical", 0.0, duration).set_delay(pause_between_cycles)
+
+func _on_scrolling():
+	if tween.is_running():
+		tween.pause()
+		await get_tree().create_timer(idle_resume_delay).timeout
+		tween.play()
+
+func _ready() -> void:
+	# Wait for layout calculations
+	await get_tree().process_frame
+	
+	# Connect to scroll event
+	var scroll_bar = get_v_scroll_bar()
+	scroll_bar.scrolling.connect(_on_scrolling)
+	
+	# Initialize tween
+	_start_auto_scroll()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
